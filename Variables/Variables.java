35aeafcbeb0feb7a71b1acc83650248710984a3e
Prathamjeet Singh <psingh@email.sc.edu>
2021-12-13 18:50:12 -0500

    variables

---
+++ Variables/Variables.java
@@ -0,0 +1,19 @@
+package Variables;
+/*
+    Primitive(8 types, stores data, can only hold 1 value, less memory, faster) - Boolean, Int, Float, char
+    Reference(user defined, stores address, can hold any number of values, more memory consuming, slower) - String
+*/
+public class Variables {
+    public static void main(String[] args){
+        int x;      //declaration
+        x = 3;      //assignment
+        int y = 5;  //initialization
+        byte byt = 120;
+        long z = 12382182173981293L;
+        float pi = 3.14f;
+        double fullPi = 3.14217382179;
+        boolean bool = false;
+        char c = 'a';
+        String myName = "Ishu";
+    }
+}
