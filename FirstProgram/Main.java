eb992d580afe0105f14d2f5178f5a8a56c60e126
Prathamjeet Singh <psingh@email.sc.edu>
2021-12-13 18:36:39 -0500

    first

---
+++ FirstProgram/Main.java
@@ -0,0 +1,11 @@
+package FirstProgram;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Practicing");
+        System.out.println("\tPracticing");
+        System.out.println("Practicing\n new line above");
+        System.out.println("\"Practicing\"");
+        System.out.println("\"\"Practicing\"\"");
+    }
+}
