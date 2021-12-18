a92254716a2c6b51b045aad4429701127668682b
IshuSingh1 <psingh@email.sc.edu>
2021-12-17 20:13:17 -0500

    if statement

--- IfStatements/Main.java
+++ IfStatements/Main.java
@@ -2,13 +2,13 @@ package IfStatements;
 
 public class Main {
     public static void main(String[] args){
-        int age = 17;
-        if(age>=18){
-            System.out.println("Adult");
-        }else if(age<18){
+        int age = 19;
+        if(age<13){
             System.out.println("Not an adult");
+        }else if(age<18){
+            System.out.println("Teen");
         }else{
-            System.out.print("Invalid");
+            System.out.print("Adult");
         }
     }
 }
