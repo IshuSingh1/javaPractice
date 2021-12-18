2b9cd314181646e9443cac32d1e6a09d88f75433
IshuSingh1 <psingh@email.sc.edu>
2021-12-18 01:58:48 -0500

    methods

---
+++ Methods/Main.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Main {
+    public static void main(String[] args) {
+        sayHello("Ishu");
+    }
+
+    static void sayHello(String name){
+        System.out.println("Hello "+name);
+    }
+}
