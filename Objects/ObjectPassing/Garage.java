bd44adf6ece78a21fa7f73eac709ba257aafedd3
IshuSingh1 <psingh@email.sc.edu>
2021-12-19 16:05:17 -0500

    object passing

---
+++ Objects/ObjectPassing/Garage.java
@@ -0,0 +1,8 @@
+package Objects.ObjectPassing;
+
+public class Garage {
+    
+    void park(Car car){
+        System.out.println("car name is: "+ car.name);
+    }
+}
---
+++ Objects/ObjectPassing/Main.java
@@ -0,0 +1,10 @@
+package Objects.ObjectPassing;
+
+public class Main {
+    public static void main(String[] args) {
+        Garage garage = new Garage();
+        Car car = new Car("BMW");
+
+        garage.park(car);
+    }
+}
